ae659ae7b9cbdb6e19acf4ebc99859aef9c4cd56
Karla Lazarte <karlalazarte@stephens-mbp.myfiosgateway.com>
2023-09-20 19:30:41 -0400

    controller route order switched

--- controllers/bookmarkController.js
+++ controllers/bookmarkController.js
@@ -12,24 +12,6 @@ router.get ("/", async (req,res) =>{
      }
 })
 
-//CREATE
-router.post("/", async (req,res) => {
-    try {
-        res.json( await Bookmark.create(req.body))
-    }catch (error){
-        res.status(400).json(error)
-    }
-})
-
-//SHOW
-router.get("/:id", async (req,res)=>{
-    try{
-        res.json(await Bookmark.findById(req.params.id));
-    }catch (error){
-        res.status(400).json(error)
-    }
-})
-
 //DELETE
 router.delete("/:id", async(req,res) =>{
     try {
@@ -45,6 +27,24 @@ router.put("/:id", async (req,res) =>{
         res.json(await Bookmark.findByIdAndUpdate(req.params.id, req.body,{ new:true}))
     }catch{
         req.status(400).json(error)
+    }
+})
+
+//CREATE
+router.post("/", async (req,res) => {
+    try {
+        res.json( await Bookmark.create(req.body))
+    }catch (error){
+        res.status(400).json(error)
+    }
+})
+
+//SHOW
+router.get("/:id", async (req,res)=>{
+    try{
+        res.json(await Bookmark.findById(req.params.id));
+    }catch (error){
+        res.status(400).json(error)
     }
 })
 module.exports = router;
