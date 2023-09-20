97031623ff7d9a85ed1c0de7137c0d8dc8f7c4f5
mconil <mconil5988@gmail.com>
2023-09-20 14:21:56 -0400

    backend completed

---
+++ controllers/bookmarkController.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const Bookmark = require('../models/bookmark');
+const router = express.Router();
+
+//INDUCES
+//INDEX
+router.get ("/", async (req,res) =>{
+     try{
+        res.json(await Bookmark.find())
+     }catch(error){
+        res.status(400).json(error)
+     }
+})
+
+//CREATE
+router.post("/", async (req,res) => {
+    try {
+        res.json( await Bookmark.create(req.body))
+    }catch (error){
+        res.status(400).json(error)
+    }
+})
+
+//SHOW
+router.get("/:id", async (req,res)=>{
+    try{
+        res.json(await Bookmark.findById(req.params.id));
+    }catch (error){
+        res.status(400).json(error)
+    }
+})
+
+//DELETE
+router.delete("/:id", async(req,res) =>{
+    try {
+        res.json(await Bookmark.findByIdAndDelete(req.params.id))
+    }catch(error){
+        res.status(400).json(error)
+    }
+})
+
+//UPDATE
+router.put("/:id", async (req,res) =>{
+    try{
+        res.json(await Bookmark.findByIdAndUpdate(req.params.id, req.body,{ new:true}))
+    }catch{
+        req.status(400).json(error)
+    }
+})
+module.exports = router;
