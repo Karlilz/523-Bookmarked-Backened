66d06c4d1a62d2e3d393115986a6e24ede743769
Karla Lazarte <karlalazarte@stephens-mbp.myfiosgateway.com>
2023-09-22 12:55:04 -0400

    app.use cors added to server.js file

--- server.js
+++ server.js
@@ -1,20 +1,19 @@
-const express = require('express');
+require("dotenv").config();
+const express = require ('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
-require('dotenv').config();
-const bookmarkController = require('./controllers/bookmarkController');
-const morgan = require('morgan');
-const cors= require ('cors');
+const bookmarkController = require('./controllers/bookmarkController')
+const morgan = require("morgan");
+const cors = require("cors");
 
 app.use(cors());
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(morgan('tiny'));
+app.use(morgan("tiny"));
+app.use(express.urlencoded({ extended:true}));
 app.use('/bookmark', bookmarkController);
 
-app.get('/', (req, res) => {
-    res.send("Hello world!")
+app.get("/", (req,res) =>{
+    res.send("Testing bookmark app")
 })
 
-app.listen(PORT,() => console.log(`Here were are on ${PORT}`))
-
+app.listen(PORT,() => console.log(`Running on' ${PORT}`))
