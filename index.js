9f81da03ff2b911c39edb6958028d4c8249cbf0e
mconil <mconil5988@gmail.com>
2023-09-23 09:15:02 -0400

    updated to make front endwork

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const bookmarkController = require('./controllers/bookmarkController')
 const morgan = require("morgan");
+const cors = require('cors')
 
+app.use(cors());
 app.use(morgan("tiny"));
 app.use(express.urlencoded({ extended:true}));
+app.use (express.json());
 
 app.get("/", (req,res) =>{
     res.send("Testing bookmark app")
